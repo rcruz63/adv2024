3e9497085b493ba3ba0689fd97fa0e7b25a2e1bc
Raul <rpublico@gmail.com>
2024-12-10 07:42:43 +0000

    Añadidos archivos y enlaces para el día 10

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Python Repo For the code of the game Advent of Code 2024
 7. [Dia 7](./README_DIA7_ES.md)
 8. [Dia 8](./README_DIA8_ES.md)
 9. [Dia 9](./README_DIA9_ES.md)
+10. [Dia 10](./README_DIA10_ES.md)
 ## English
 
 1. [Day 1](./README_DIA1_EN.md)
---
+++ adv2024/dia10/dia10.py
@@ -0,0 +1,64 @@
+import os
+import time
+
+def vprint(verbose, *args):
+    if verbose:
+        print(*args)
+
+def dia10_1(data, verbose: bool = False):
+    """ Función principal del día 10-1. """
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    data_path = os.path.join(current_dir, data)
+    if verbose:
+        print(f'Opening file {data_path}')
+
+    # Leer el archivo
+    with open(data_path, "r", encoding="utf-8") as file:
+        # Leer cada linea del archivo
+        result = 0
+        for line in file:
+            # Decodificar la linea
+            pass
+            
+    # Imprimir el resultado
+
+    print(f'resultado dia 10 - 1 = "{result}"')
+    return result
+
+
+def dia10_2(data, verbose: bool = False):
+    """ Función principal del día 10-2. """
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    data_path = os.path.join(current_dir, data)
+    if verbose:
+        print(f'Opening file {data_path}')
+
+    # Leer el archivo
+    with open(data_path, "r", encoding="utf-8") as file:
+        # Leer cada linea del archivo
+        result = 0
+        num_line = 1
+        jugadas = []
+        for line in file:
+            # Decodificar la linea
+            pass
+
+    # Imprimir el resultado
+
+    print(f'resultado dia 10 - 2 = "{result}"')
+    return result
+
+if __name__ == "__main__":
+    start_time = time.time()
+    assert dia10_1("test10_1.txt", verbose=True) == 11, "Error se esperaba 11."
+    # dia10_1("data10_1.txt", verbose=False)
+    end_time = time.time()
+    print(f"Tiempo de ejecución parte 1: {end_time - start_time:.4f} segundos")
+    start_time = time.time()
+    # assert dia10_2("test10_1.txt", verbose=True) == 31, "Error se esperaba 9."
+    # dia10_2("data10_1.txt", verbose=False)
+    end_time = time.time()
+    print(f"Tiempo de ejecución parte 2: {end_time - start_time:.4f} segundos")
+
